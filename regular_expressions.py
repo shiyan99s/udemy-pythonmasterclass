f3d5b88a9a0b56f84f74d5a8c74fdc803474df7d
shiyan99s <shiyan99@gmail.com>
2020-06-10 11:51:02 +0530

    dot metacharacter

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,4 +1,6 @@
-#importing re module
+"""
+    importing re module
+"""
 
 import re
 
@@ -18,3 +20,25 @@ else:
 # re.findall
 print(re.findall(pattern, "baconeggseggsbacon"))
 
+"""
+    implementing sub module from RE module
+"""
+
+import re
+
+mystring = "My name is John and my mother named me John because John Floyd was her favourite singer"
+
+pattern = r"John"     # 'r' means raw, pattern should be a raw format and should be stored in a variable
+
+variable = re.sub(pattern, "Shiyan", mystring)
+
+print(variable)
+
+
+"""
+    re.match
+"""
+
+lmao = r"lo.....h"
+if re.match(lmao, "logitech"):
+    print("TRUE")
