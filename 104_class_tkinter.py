8e96139b90ff9e1280fa4c3cf6b6cf173f798202
shiyan99s <shiyan99@gmail.com>
2020-06-17 15:47:10 +0530

    created class with operatable buttons with actual code

---
+++ 104_class_tkinter.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+class MyButtons:
+
+    #Creating an __init__ method
+
+    def __init__(self, rootone):
+        frame = Frame(rootone)
+        frame.pack()
+
+        self.printmessage = Button(frame, text="Click here", bg="Red", fg="White", command=self.printmessage)
+        self.printmessage.pack()
+
+        self.quitbutton = Button(frame, text="Exit", command=frame.quit)
+        self.quitbutton.pack()
+
+    def printmessage(self):
+        print("You have clicked button")
+        
+
+root = Tk()        
+#This line will create an object of class MyButtons
+object1 = MyButtons(root)        
+        
+root.mainloop()
