cec024076829e273bcc41d5f3d0916fbbba7bdb1
shiyan99s <shiyan99@gmail.com>
2020-06-18 19:01:57 +0530

    added toolbat to dropwn project

--- 105_dropdown_menu_tkinter.py
+++ 105_dropdown_menu_tkinter.py
@@ -6,6 +6,9 @@ def function1():
 def function2():
     print("Edit menu clicked")
 
+def function3():
+    print("You have clicked toolbar button")
+
 root = Tk()
 
 #This line will create an object of an pre-existing class Menu
@@ -30,5 +33,18 @@ submenu.add_command(label="Open File", command=function1)
 submenu1.add_command(label="Undo", command=function2)
 submenu1.add_command(label="Redo", command=function2)
 submenu1.add_command(label="Cut", command=function2)
+
+#This line will create a toolbar
+toolbar1 = Frame(root, bg="pink")
+button1 = Button(toolbar1, text="Save as PDF", command=function3)
+button1.pack(side=LEFT, padx=2, pady=3)
+
+button2 = Button(toolbar1, text="Print PDF", command=function3)
+button2.pack(side=LEFT, padx=4, pady=4)
+
+button3 = Button(toolbar1, text="Exit", command=toolbar1.quit)
+button3.pack(side=LEFT, padx=4, pady=4)
+
+toolbar1.pack(side=TOP, fill=X)
 
 root.mainloop()
